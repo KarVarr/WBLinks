429ceba1754eb5b121a53298c0495eabb3222516
KarVarr <megatr9n@gmail.com>
2023-10-08 17:44:13 +0400

    added targer _blank for links

--- script.js
+++ script.js
@@ -2,17 +2,16 @@ function convertToLinks() {
   const inputText = document.getElementById('inputText').value;
   const outputLinks = document.getElementById('outputLinks');
 
-
   const lines = inputText.split('\n');
 
-  
   outputLinks.innerHTML = '';
   lines.forEach(function (line, index) {
     const trimmedLine = line.trim();
     if (trimmedLine !== '') {
       const linkElement = document.createElement('a');
       linkElement.href = trimmedLine;
-      linkElement.textContent = `link h&m ${index + 1}`;
+      linkElement.textContent = `LINK H&M №: ${index + 1}`;
+      linkElement.target = '_blank';
       outputLinks.appendChild(linkElement);
 
       const lineBreak = document.createElement('br');
