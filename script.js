b9651b65ee73d709c5ab5225c43424b19c9fa929
KarVarr <megatr9n@gmail.com>
2023-10-09 19:21:17 +0400

    added localStorage

--- script.js
+++ script.js
@@ -5,21 +5,26 @@ function convertToLinks() {
   const outputLinks = document.getElementById('outputLinks');
 
   const lines = inputText.split('\n');
+  const savedLinks = [];
 
   outputLinks.innerHTML = '';
+
   lines.forEach(function (line, index) {
     const trimmedLine = line.trim();
     if (trimmedLine !== '') {
       const linkElement = document.createElement('a');
       linkElement.href = trimmedLine;
       linkElement.textContent = `LINK H&M №: ${index + 1}`;
-      linkElement.target = '_blank';
       outputLinks.appendChild(linkElement);
 
       const lineBreak = document.createElement('br');
       outputLinks.appendChild(lineBreak);
+
+      savedLinks.push(trimmedLine);
     }
   });
+
+  localStorage.setItem('savedLinks', JSON.stringify(savedLinks));
 }
 
 function deleteAllLinks() {
@@ -27,4 +32,15 @@ function deleteAllLinks() {
   const outputLinks = document.getElementById('outputLinks');
   inputText.value = '';
   outputLinks.remove();
+
+  localStorage.removeItem('savedLinks');
 }
+
+window.addEventListener('load', function () {
+  const savedLinks = JSON.parse(localStorage.getItem('savedLinks'));
+  if (savedLinks && savedLinks.length > 0) {
+    const inputText = document.getElementById('inputText');
+    inputText.value = savedLinks.join('\n');
+    convertToLinks(); 
+  }
+});
